415c706188b79889a73662ad609d00da0c3830fe
md hasnahin sehan <92535312+shehanaip@users.noreply.github.com>
2026-05-20 00:57:47 +0600

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://invent-yfwy.onrender.com/api",
 });
 
 // Auto attach token
