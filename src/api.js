f0e10f052d8d535cd58619f934f59498b22db14a
md hasnahin sehan <92535312+shehanaip@users.noreply.github.com>
2026-05-20 00:31:33 +0600

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
-// auto attach token
+// Auto attach token
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
